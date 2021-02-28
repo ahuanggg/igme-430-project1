d93c92bae223cbc152096c1152795b33c19b0311
ahuanggg <ah6595@rit.edu>
2021-02-28 03:24:33 -0500

    project 1 draft

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,27 +1,78 @@
 const fs = require('fs');
 
+const home = fs.readFileSync(`${__dirname}/../client/home.html`);
 const error = fs.readFileSync(`${__dirname}/../client/error.html`);
 const css = fs.readFileSync(`${__dirname}/../client/style.css`);
-const jokeclient = fs.readFileSync(`${__dirname}/../client/joke-client.html`);
+const motivation = fs.readFileSync(`${__dirname}/../client/motivation.html`);
+const recipe = fs.readFileSync(`${__dirname}/../client/recipe.html`);
+const admin = fs.readFileSync(`${__dirname}/../client/admin.html`);
+const dog = fs.readFileSync(`${__dirname}/../client/dog.jpg`);
+
+//for the calender
+const calcss = fs.readFileSync(`${__dirname}/../client/fullcal/main.css`);
+const caljs = fs.readFileSync(`${__dirname}/../client/fullcal/main.js`);
+
+const getMotivation = (request, response) => {
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.write(motivation);
+	response.end();
+};
 
 const getError = (request, response) => {
-  response.writeHead(404, { 'Content-Type': 'text/html' });
-  response.write(error);
-  response.end();
+	response.writeHead(404, { 'Content-Type': 'text/html' });
+	response.write(error);
+	response.end();
 };
 
 const getCSS = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/css' });
-  response.write(css);
-  response.end();
+	response.writeHead(200, { 'Content-Type': 'text/css' });
+	response.write(css);
+	response.end();
 };
 
-const getClient = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(jokeclient);
-  response.end();
+const getHome = (request, response) => {
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.write(home);
+	response.end();
+};
+
+const getDog = (request, response) => {
+	response.writeHead(200, { 'Content-Type': 'image/jpg' });
+	response.write(dog);
+	response.end();
+};
+const getRecipe = (request, response) => {
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.write(recipe);
+	response.end();
+};
+
+const getAdmin = (request, response) => {
+	response.writeHead(200, { 'Content-Type': 'text/html' });
+	response.write(admin);
+	response.end();
+};
+
+const getCalCSS = (request, response) => {
+	response.writeHead(200, { 'Content-Type': 'text/css' });
+	response.write(calcss);
+	response.end();
+};
+
+const getCalJS = (request, response) => {
+	response.writeHead(200, { 'Content-Type': 'application/javascript' });
+	response.write(caljs);
+	response.end();
 };
 
 module.exports.getError = getError;
 module.exports.getCSS = getCSS;
-module.exports.getClient = getClient;
+module.exports.getHome = getHome;
+module.exports.getMotivation = getMotivation;
+module.exports.getDog = getDog;
+module.exports.getAdmin = getAdmin;
+module.exports.getRecipe = getRecipe;
+
+//for the calender
+module.exports.getCalCSS = getCalCSS;
+module.exports.getCalJS = getCalJS;
