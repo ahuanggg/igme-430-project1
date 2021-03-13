42f2bf49ea6658e63bf4cd1dd19b3915fd6bb7a6
ahuanggg <ah6595@rit.edu>
2021-03-13 00:22:05 -0500

    draft

--- src/index.js
+++ src/index.js
@@ -9,59 +9,99 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const htmlHandler = require('./htmlResponses.js');
 const responseHandler = require('./responses.js');
+const { parse } = require('path');
+
+const handlePost = (request, response, parsedUrl) => {
+	// console.dir('in handle');
+	// console.dir(parsedUrl.pathname);
+	if (parsedUrl.pathname === '/add-quote') {
+		const body = [];
+
+		request.on('error', (err) => {
+			// console.dir('in err');
+			// console.dir(err);
+			repsonse.statusCode = 400;
+			repsonse.end;
+		});
+
+		request.on('data', (chunk) => {
+			// console.dir('in data');
+			body.push(chunk);
+		});
+
+		request.on('end', () => {
+			// console.dir(body);
+			const bodyString = Buffer.concat(body).toString();
+			// console.log(bodyString);
+			const bodyParams = query.parse(bodyString);
+			// console.dir(bodyParams);
+
+			responseHandler.addQuote(request, response, bodyParams);
+		});
+	}
+	// console.dir('outside if');
+};
 
 const urlStruct = {
-  '/random-quote': responseHandler.getRandomQuoteResponse,
-  '/': htmlHandler.getHome,
-  '/style.css': htmlHandler.getCSS,
-  '/motivation.html': htmlHandler.getMotivation,
-  '/dog.jpg': htmlHandler.getDog,
-  '/admin.html': htmlHandler.getAdmin,
-  '/suggestions.html': htmlHandler.getSuggestion,
+	'/random-quote': responseHandler.getRandomQuoteResponse,
+	'/add-quote': responseHandler.addQuote,
+	'/': htmlHandler.getHome,
+	'/style.css': htmlHandler.getCSS,
+	'/motivation.html': htmlHandler.getMotivation,
+	'/dog.jpg': htmlHandler.getDog,
+	'/admin.html': htmlHandler.getAdmin,
+	'/suggestions.html': htmlHandler.getSuggestion,
 
-  // for the calander css and js
-  '/full-cal-css.css': htmlHandler.getCalCSS,
-  '/full-cal-js.js': htmlHandler.getCalJS,
-  notFound: htmlHandler.getError,
+	// for the calander css and js
+	'/full-cal-css.css': htmlHandler.getCalCSS,
+	'/full-cal-js.js': htmlHandler.getCalJS,
+	notFound: htmlHandler.getError,
 };
 
 const onRequest = (request, response) => {
-  // console.log(request.headers);
+	// console.log(request.headers);
 
-  const parsedUrl = url.parse(request.url);
-  const { pathname } = parsedUrl;
+	const parsedUrl = url.parse(request.url);
+	const { pathname } = parsedUrl;
 
-  const httpMethod = request.method;
+	//handle POST
+	if (request.method === 'POST') {
+		// console.dir('in post');
+		handlePost(request, response, parsedUrl);
+		return;
+	}
 
-  let acceptedTypes = request.headers.accept && request.headers.accept.split(',');
-  acceptedTypes = acceptedTypes || [];
+	const httpMethod = request.method;
 
-  const params = query.parse(parsedUrl.query);
-  // const { limit } = params;
+	let acceptedTypes = request.headers.accept && request.headers.accept.split(',');
+	acceptedTypes = acceptedTypes || [];
 
-  if (pathname === '/') {
-    urlStruct[pathname](request, response);
-  } else if (pathname === '/style.css') {
-    urlStruct[pathname](request, response);
-  } else if (pathname === '/motivation.html') {
-    urlStruct[pathname](request, response);
-  } else if (pathname === '/dog.jpg') {
-    urlStruct[pathname](request, response);
-  } else if (pathname === '/admin.html') {
-    urlStruct[pathname](request, response);
-  } else if (pathname === '/suggestions.html') {
-    urlStruct[pathname](request, response);
-  } else if (pathname === '/random-quote') {
-    urlStruct[pathname](request, response, params, acceptedTypes, httpMethod);
-  } else if (pathname === '/full-cal-css.css') {
-    // for the calenders
-    urlStruct[pathname](request, response);
-  } else if (pathname === '/full-cal-js.js') {
-    // for the calenders
-    urlStruct[pathname](request, response);
-  } else {
-    urlStruct.notFound(request, response);
-  }
+	const params = query.parse(parsedUrl.query);
+	// const { limit } = params;
+
+	if (pathname === '/') {
+		urlStruct[pathname](request, response);
+	} else if (pathname === '/style.css') {
+		urlStruct[pathname](request, response);
+	} else if (pathname === '/motivation.html') {
+		urlStruct[pathname](request, response);
+	} else if (pathname === '/dog.jpg') {
+		urlStruct[pathname](request, response);
+	} else if (pathname === '/admin.html') {
+		urlStruct[pathname](request, response);
+	} else if (pathname === '/suggestions.html') {
+		urlStruct[pathname](request, response);
+	} else if (pathname === '/random-quote') {
+		urlStruct[pathname](request, response, params, acceptedTypes, httpMethod);
+	} else if (pathname === '/full-cal-css.css') {
+		// for the calenders
+		urlStruct[pathname](request, response);
+	} else if (pathname === '/full-cal-js.js') {
+		// for the calenders
+		urlStruct[pathname](request, response);
+	} else {
+		urlStruct.notFound(request, response);
+	}
 };
 
 http.createServer(onRequest).listen(port);
